51c8429766f3c26be8deda92e0a3a1688e294619
Huy Tran <thkhuy@tma.com.vn>
2025-08-11 14:11:17 +0700

    change to nuxt 3

--- app/plugins/push.client.ts
+++ app/plugins/push.client.ts
@@ -15,7 +15,7 @@ export default defineNuxtPlugin(async () => {
       applicationServerKey: urlBase64ToUint8Array('BK4gbl_eAnY702WNd1vMOah_xxtY7qh1DChs4a4sZ86Q7-eNfuUU_DwRwF-6tDJJZnV7N2tjAxrran0mfAEO50I')
     })
 
-    await fetch('https://a0f08f08fc3a9960c41748c270e640f8.serveo.net/save-sub', {
+    await fetch('https://189fcf81c30f047d598c372968c46c67.serveo.net/save-sub', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(subscription)
